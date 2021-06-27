759da1a032e1eb8afdf6873139e3a75145c04c0e
Augustine-maker <alaimirjigit@mail.ru>
2021-06-27 18:28:37 +0600

    Суранбаев Миржигит Андроид Разработка

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,35 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         System.out.println("Введите ваше имя - ");
-        Scanner name =new Scanner(System.in);
-        String string = name.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите ваш возраст - ");
-        Scanner age = new Scanner(System.in);
-        String string1 = age.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("Введите в градусах погоду на улице - ");
-        Scanner weather = new Scanner(System.in);
+        int age = generateRandomAge(0, 107);
 
+        int weather = 25;
 
-        System.out.println(name.next() + age.next() + weather.next());
+        if (age >= 20 && age <= 45 && weather <= 30 && weather >= -20) {
+            System.out.println(name + " ,Вам " + age + " гулять можно");
+        } else if (age <= 20 && weather >= 0 && weather <= 28) {
+            System.out.println(name + " ,Вам " + age + " гулять можно");
+        } else if (age >= 45 && weather >= -10 && weather <= 25) {
+            System.out.println(name + " ,Вам " + age + " гулять можно");
+        } else {
+            System.out.println(" Вам гулять нельзя");
+        }
+    }
 
+    public static int generateRandomAge(int min, int max) {
+        Random randomAge = new Random();
+        return randomAge.nextInt(max - min + 1) + min;
     }
 }
